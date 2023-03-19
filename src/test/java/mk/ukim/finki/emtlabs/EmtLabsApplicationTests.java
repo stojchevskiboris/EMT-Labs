6c6572908f9ae5f33b35297e15f876a7cf2cdd3e
Boris Stojchevski <stojcevskib.02@gmail.com>
2023-03-19 13:32:42 +0100

    Initial commit

---
+++ src/test/java/mk/ukim/finki/emtlabs/EmtLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emtlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
